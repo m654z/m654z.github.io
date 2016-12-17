9d17138022778c59cde9787f795e0e19e896f6a9
William Muhu <wmuhucraft@gmail.com>
2016-12-17 14:58:28 +0200

    Stuff

---
+++ memedump/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome to the meme dump.";
+echo "This is where I upload all the memes that are clogging up my Pictures folder.";
+echo "Feel free to browse them.";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
